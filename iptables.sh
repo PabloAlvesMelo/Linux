d3fc399d0e58afcd4e1ac7bbb809e0c16a836e32
Pablo Melo <pabloalvesmelo@gmail.com>
2022-01-05 16:44:17 -0300

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -2,11 +2,13 @@
 # Parametros:
 #===================================================================
 ipt='iptables'
+# => lan(local)
 eth1='eth0'
-eth2='eth1'
-lan='192.168.3.169'
-wan='1.1.1.1'
+ip1='192.168.3.169'
 net='192.168.0.0/22'
+# => wan(internet)
+eth2='eth1' 
+ip2='1.1.1.1'
 _ssh='8899'
 _mysql='3306'
 ncon='20'
@@ -25,9 +27,40 @@ $ipt -P OUTPUT ACCEPT
 #$ipt -P OUTPUT DROP
 $ipt -P FORWARD DROP
 #====================================================================
+echo -e "\e[96m-> CAREGANDO MODULOS -------------------------------------\e[92m OK \e[0m"
+modprobe iptable_nat
+modprobe iptable_filter
+modprobe iptable_mangle
+modprobe iptable_raw
+modprobe ip_tables
+modprobe ip_conntrack
+modprobe ipt_LOG
+modprobe ipt_MASQUERADE
+modprobe ipt_limit
+modprobe ipt_state
+#====================================================================
+echo -e "\e[96m-> ENCAMINHAMENTO DE PACOTES -----------------------------\e[92m OK \e[0m"
+echo 1 >/proc/sys/net/ipv4/ip_forward
+echo 1 > /proc/sys/net/ipv4/tcp_syncookies
+#====================================================================
+echo -e "\e[96m-> BLOQUEIO DE REDE (SEGURANCA) --------------------------\e[92m OK \e[0m"
+$ipt -A FORWARD -p tcp --syn -m limit --limit 2/s -j ACCEPT
+$ipt -A INPUT -p tcp --dport 80 -m limit --limit 100/minute --limit-burst 200 -j ACCEPT
+echo 1 >/proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+echo 1 >/proc/sys/net/ipv4/conf/default/rp_filter
+#
+$ipt -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+$ipt -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+$ipt -A INPUT -i $eth1 -s $ip1 -j DROP
+#$ipt -A INPUT -i $eth2 -s $ip2 -j DROP
+#====================================================================
 echo -e "\e[96m-> ACEITAR PING ------------------------------------------\e[92m OK \e[0m"
 $ipt -A INPUT -p icmp -j ACCEPT
 #$ipt -A OUTPUT -p icmp -j ACCEPT
+#====================================================================
+#echo -e "\e[96m-> COMPARTILHAMETO DE WAN --------------------------------\e[92m OK \e[0m"
+#$ipt -t nat -A POSTROUTING -s $net -j MASQUERADE
+#$ipt -A FORWARD -i $eth1 -j ACCEPT
 #====================================================================
 echo -e "\e[96m-> CONEXAO REDE LO ---------------------------------------\e[92m OK \e[0m"
 $ipt -A INPUT  -i lo -j ACCEPT
@@ -96,27 +129,14 @@ all=('10.0.0.0/8' '172.16.0.0/16' '192.168.0.0/16' '177.125.217.137/29' '177.72.
 echo -e "\e[96m-> ESTABILIZA CONEXOES ABERTAS ---------------------------\e[92m OK \e[0m"
 $ipt -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 
 #====================================================================
-echo -e "\e[96m-> CAREGANDO MODULOS -------------------------------------\e[92m OK \e[0m"
-modprobe iptable_nat
-modprobe ip_tables
-modprobe ip_conntrack
-modprobe ipt_LOG
-modprobe ipt_MASQUERADE
-modprobe ipt_limit
-modprobe ipt_state
-#====================================================================
-echo -e "\e[96m-> ENCAMINHAMENTO DE PACOTES -----------------------------\e[92m OK \e[0m"
-echo 1 >/proc/sys/net/ipv4/ip_forward
-echo 1 > /proc/sys/net/ipv4/tcp_syncookies
-#====================================================================
-echo -e "\e[96m-> BLOQUEIO DE REDE (SEGURANCA) --------------------------\e[92m OK \e[0m"
-$ipt -A FORWARD -p tcp --syn -m limit --limit 2/s -j ACCEPT
-$ipt -A INPUT -p tcp --dport 80 -m limit --limit 100/minute --limit-burst 200 -j ACCEPT
-echo 1 >/proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
-echo 1 >/proc/sys/net/ipv4/conf/default/rp_filter
-#====================================================================
 echo -e "\e[96m-> LIMITANDO NUMERO DE CONEXAO ---------------------------\e[92m OK \e[0m"
 $ipt -A INPUT -p tcp --syn --dport $_ssh -m connlimit --connlimit-above $ncon -j REJECT
 echo -e "Conexoes simultaneas de um mesmo ip \e[93m$ncon\e[0m"
+#====================================================================
+echo -e "\e[96m-> REDIRECIONAMENTO DE PORTAS ----------------------------\e[92m OK \e[0m"
+#$ipt -t nat -A PREROUTING -d $ip2 -p tcp --dport 8291 -j DNAT --to 192.168.1.225
+#$ipt -t nat -A POSTROUTING -d 192.168.1.225 -p tcp --dport 8291 -j SNAT --to $ip2
 
+# Bloquieo de MAC
+#$ipt -A INPUT -m mac --mac-source 00:00:00:00:00:00 -j DROP
 systemctl restart docker
